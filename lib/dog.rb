a5ccb250aa58d65bb7404edfc7193cf9721756b0
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-09 22:46:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def initialize(breed)
+    @breed = breed 
+  end 
+  def breed=(breed= "Mutt")
+    @breed = breed 
+  end 
+  def breed
+    @breed
+  end 
+  def name=(name)
+    @name = name 
+  end 
+  def name 
+    @name
+  end 
+end 
+  
